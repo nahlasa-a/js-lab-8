67b264382872db36823a5dfcb078e495ec5971e1
nahlasa-a <sayedahmad.nahla@gmail.com>
2025-04-05 18:40:59 -0400

    creating strings, number, boolean, and displaying student info

---
+++ script.js
@@ -0,0 +1,18 @@
+const student = {
+    name: "Nahla Sayed Ahmad",
+    age: 22,
+    enrolled: true,
+    studentId: "n01432466",
+    program: "Computer Programming & Analysis",
+    college: "Humber Polytechnic Institute", 
+    courses: ["Algorithms", "Database Design", "Operating Systems", "Web Programming", "JavaScript"],
+    
+    displayInfo: function() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Age: ${this.age}`);
+        console.log(`Enrolled: ${this.enrolled ? "Yes" : "No"}`);
+        console.log(`Courses: ${this.courses.join(", ")}`);
+    }
+};
+
+student.displayInfo();
